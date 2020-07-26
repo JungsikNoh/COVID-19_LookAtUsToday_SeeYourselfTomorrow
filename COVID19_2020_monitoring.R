65b46fc41eb1077cbd0ff6b58c283e49f2c889be
JungsikNoh <jungsik.noh@utsouthwestern.edu>
2020-07-26 12:19:34 -0500

    07/25 update

--- COVID19_2020_monitoring.R
+++ COVID19_2020_monitoring.R
@@ -14,7 +14,7 @@
 ## source functions 
 t1 = Sys.time()
 #curDate = Sys.Date(); print(curDate)
-curDate = '2020-07-24'
+curDate = '2020-07-25'
 
 #setwd('C:/githubClone/COVID-19_LookAtUsToday_SeeYourselfTomorrow')
 print(getwd())
